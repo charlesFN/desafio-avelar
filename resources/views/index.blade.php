687bbaf3832bc512892b0e82192aa224d7ad840c
charlesNF <charles.comercial.nunes@gmail.com>
2025-04-30 15:41:41 -0300

    instalação do bootstrap 5

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Isso é o título de um card</h3>
+            </div>
+            <div class="card-body">
+                Isso é o body de um card.
+            </div>
+            <div class="card-footer">
+                Isso é o footer de um card.
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('web', 'auth')->group(function () {
-    Route::get('/desafio-avelar', [Controller::class, 'index'])->name('desafio.avelar.index');
+Route::get('/desafio-avelar', function() {
+    return view('index');
 });
+
+/* Route::middleware('web', 'auth')->group(function () {
+    Route::get('/desafio-avelar', [Controller::class, 'index'])->name('desafio.avelar.index');
+}); */
