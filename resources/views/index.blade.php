329a0e3067b61a9469921ba182f3fb78bf218dcc
charlesNF <charles.comercial.nunes@gmail.com>
2025-04-30 16:46:04 -0300

    criação de formulário de cadastro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,17 +10,62 @@
 </head>
 <body>
     <div class="container">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Isso é o título de um card</h3>
+        <form action="{{ route('desafio.avelar.store') }}" method="post">
+            @csrf
+
+            <div class="mb-3">
+                <label for="nome">Nome <span class="text-danger">*</span></label>
+                <input required type="text" name="nome" id="nome" class="form-control" maxlength="150">
             </div>
-            <div class="card-body">
-                Isso é o body de um card.
+            <div class="mb-3">
+                <label for="idade">Idade <span class="text-danger">*</span></label>
+                <div class="input-group">
+                    <input required type="number" name="idade" class="form-control" id="idade" min="0" step="1">
+                    <span class="input-group-text">Anos</span>
+                </div>
             </div>
-            <div class="card-footer">
-                Isso é o footer de um card.
+            <div class="mb-3">
+                <label for="cep">CEP <span class="text-danger">*</span></label>
+                <input required type="text" name="cep" id="cep" class="form-control" maxlength="13">
             </div>
-        </div>
+            <div class="mb-3">
+                <label for="cidade">Cidade <span class="text-danger">*</span></label>
+                <input required type="text" name="cidade" id="cidade" class="form-control" maxlength="100">
+            </div>
+            <div class="mb-3">
+                <label for="estado">Estado <span class="text-danger">*</span></label>
+                <input required type="text" name="estado" id="estado" class="form-control" maxlength="2">
+            </div>
+            <div class="mb-3">
+                <label for="rua">Rua <span class="text-danger">*</span></label>
+                <input required type="text" name="rua" id="rua" class="form-control" maxlength="150">
+            </div>
+            <div class="mb-3">
+                <label for="bairro">Bairro <span class="text-danger">*</span></label>
+                <input required type="text" name="bairro" id="bairro" class="form-control" maxlength="100">
+            </div>
+            <div class="mb-3">
+                <label for="sexo">Sexo <span class="text-danger">*</span></label>
+                <select name="sexo" id="sexo" class="form-select">
+                    <option selected value="{{ null }}">Selecione uma opção</option>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Feminino">Feminino</option>
+                    <option value="Outro">Outro</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="salario">Salário <span class="text-danger">*</span></label>
+                <div class="input-group">
+                    <span class="input-group-text">R$</span>
+                    <input required type="number" name="salario" id="salario" class="form-control" min="0" max="999999999999.99" step="0.01">
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="anexo">Anexo (Máx. 10MB) <span class="text-danger">*</span></label>
+                <input required type="file" name="anexo" id="anexo" class="form-control" accept=".pdf, .jpg, .png" size="">
+            </div>
+            <button class="btn btn-primary">Cadastrar</button>
+        </form>
     </div>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\DesafioAvelarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/desafio-avelar', function() {
-    return view('index');
-});
+Route::get('/desafio-avelar', [DesafioAvelarController::class, 'index'])->name('desafio.avelar.index');
+Route::post('/desafio-avelar/store', [DesafioAvelarController::class, 'store'])->name('desafio.avelar.store');
 
 /* Route::middleware('web', 'auth')->group(function () {
     Route::get('/desafio-avelar', [Controller::class, 'index'])->name('desafio.avelar.index');
