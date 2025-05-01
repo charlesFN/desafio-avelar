b685da24b58ebf511be03192f568633fbcb28fd7
charlesNF <charles.comercial.nunes@gmail.com>
2025-05-01 18:18:37 -0300

    instalação de pacote para tradução do laravel

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,11 @@ use App\Http\Controllers\Controller;
 use App\Http\Controllers\DesafioAvelarController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
-Route::get('/desafio-avelar', [DesafioAvelarController::class, 'index'])->name('desafio.avelar.index');
+Route::get('/', [DesafioAvelarController::class, 'index'])->name('desafio.avelar.index');
 Route::post('/desafio-avelar/store', [DesafioAvelarController::class, 'store'])->name('desafio.avelar.store');
 Route::put('/desafio-avelar/update', [DesafioAvelarController::class, 'update'])->name('desafio.avelar.update');
 Route::delete('desafio-avelar/delete', [DesafioAvelarController::class, 'destroy'])->name('desafio.avelar.delete');
